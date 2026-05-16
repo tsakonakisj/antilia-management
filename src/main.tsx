5cd01233dcfeb9f97a5e0fe91e35361667614acb
tsakonakisj <tsakonakisj@gmail.com>
2026-05-16 13:02:47 +0300

    Add Auth/Language Providers in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { LanguageProvider } from './contexts/LanguageContext.tsx';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <LanguageProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </LanguageProvider>
   </StrictMode>
 );
